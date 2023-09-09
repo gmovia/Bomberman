b3d095b91a07e4c3183497353cba845dd24b6d5c
Guido Movia <gmovia@Guidos-MacBook-Pro.local>
2023-09-09 16:10:31 -0300

    refactor

--- src/lab.rs
+++ src/lab.rs
@@ -1,14 +1,31 @@
 use crate::{converter::Converter, element::Element};
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 pub struct Maze {
     maze: Vec<Vec<Element>>,
+    limit_x: usize,
+    limit_y: usize
 }
 
 impl Maze {
     pub fn new(maze: &str) -> Maze {
+        let matrix = Converter::string_to_matrix_object(maze);
+        let limit_x: usize = matrix.len();
+        let limit_y: usize = matrix[0].len();
         Maze {
-            maze: Converter::string_to_matrix_object(maze),
+            maze: matrix, 
+            limit_x: limit_x, 
+            limit_y: limit_y
         }
+    }
+
+    pub fn is_in_maze(&self, x: usize, y: usize) -> bool{
+        x < self.limit_x && y < self.limit_y
     }
 
     pub fn detonate(&mut self, x: usize, y: usize) -> Result<String, String> {
@@ -23,52 +40,51 @@ impl Maze {
 
     pub fn detonate_bomb(&mut self, scope: usize, x: usize, y: usize) {
         self.maze[x][y] = Element::Empty;
+        self.expand(x, y, scope, Direction::Right, false);
+        self.expand(x, y, scope, Direction::Down, false);
+        self.expand(x, y, scope, Direction::Up, true);
+        self.expand(x, y, scope, Direction::Left, true);
+    }
 
-        for row in x..x + scope + 1 {
-            if row < self.maze.len() {
-                if self.apply(row, y) == false{
-                    break
+    
+    fn expand(&mut self, x: usize, y: usize, scope: usize, direction: Direction, reverse: bool){
+        let (start, end, a, b, c, d) = match direction {
+            Direction::Up => (if scope > x { 0 } else { x - scope }, x, 0, 1, 1, 0),
+            Direction::Down => (x, x + scope, 0, 1, 1, 0),
+            Direction::Left => (if scope > y { 0 } else { y - scope }, y, 1, 0, 0, 1),
+            Direction::Right => (y, y + scope, 1, 0, 0, 1),
+        };
+        
+        if reverse == true{
+            for index in (start..=end).rev(){
+                if self.is_in_maze(x*a + index*c, y*b + index*d){
+                    if !self.apply(x*a + index*c, y*b + index*d){
+                        break;
+                    }
                 }
             }
         }
-        let start = if scope > x { 0 } else { x - scope };
-        for row in (start..x + 1).rev() {
-            if row < self.maze.len() {
-                if self.apply(row, y) == false{
-                    break
-                }
-            }
-        }
-
-        for column in y..y + scope + 1 {
-            if column < self.maze[0].len() {
-                if self.apply(x, column) == false{
-                    break
-                }
-            }
-        }
-
-        let start = if scope > y { 0 } else { y - scope };
-        for column in (start..y + 1).rev() {
-            if column < self.maze[0].len() {
-                if self.apply(x, column) == false{
-                    break
+        else{
+            for index in start..=end{
+                if self.is_in_maze(x*a + index*c, y*b + index*d){
+                    if !self.apply(x*a + index*c, y*b + index*d){
+                        break;
+                    }
                 }
             }
         }
     }
 
-    pub fn apply(&mut self, x: usize, y: usize) -> bool{
-        if let Element::Bomb(_, scope) = &self.maze[x][y] {
-            self.detonate_bomb(*scope, x, y);
+    pub fn apply(&mut self, x: usize, y: usize) -> bool {
+        match &self.maze[x][y] {
+            Element::Bomb(_, scope) => {
+                self.detonate_bomb(*scope, x, y);
+                true
+            }
+            Element::Rock | Element::Wall => false,
+            _ => true,
         }
-        if let Element::Rock = &self.maze[x][y] {
-            return false;
-        }
-
-        if let Element::Wall = &self.maze[x][y] {
-            return false;
-        }
-        return true;        
     }
+    
 }
+
