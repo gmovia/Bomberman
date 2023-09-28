b0a95a060bd2c86ac664d5d0addbd9a94724dd2f
gmovia <gmovia@fi.uba.ar>
2023-09-27 23:08:30 -0300

    fix input file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use taller1_tp1_bomberman::files::reader::Reader;
 use taller1_tp1_bomberman::files::writer::Writer;
 use taller1_tp1_bomberman::lab::Maze;
 use taller1_tp1_bomberman::utils::error::error;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,10 +15,19 @@ fn main() {
         println!("{}", ERR_ARGS);
     }
 
+    let input = Path::new(&args[1]);
     let path_input = &args[1];
     let path_output = &args[2];
-    let concatenated_path = format!("{}{}", path_output, path_input);
+    let mut file_name = "";
 
+    if let Some(name) = input.file_name(){
+        if let Some(name_str) = name.to_str(){
+            file_name = name_str;
+        }
+    }
+    
+    let concatenated_path = format!("{}{}", path_output, file_name);
+    
     let x: usize = match args[3].parse() {
         Ok(x) => x,
         Err(_) => {
@@ -25,7 +35,7 @@ fn main() {
             return;
         }
     };
-
+    
     let y: usize = match args[4].parse() {
         Ok(y) => y,
         Err(_) => {
@@ -33,7 +43,7 @@ fn main() {
             return;
         }
     };
-
+    
     match Reader::read(path_input, &mut content) {
         Ok(()) => println!("Lectura exitosa"),
         Err(_) => {
@@ -41,15 +51,15 @@ fn main() {
             return;
         }
     }
-
+    
     let new_content = content.trim_end_matches('\n');
     let mut maze: Maze = Maze::new(new_content);
-
+    
     match maze.detonate(x, y) {
         Ok(r) => result.push_str(r.as_str()),
         Err(e) => result.push_str(e.as_str()),
     }
-
+    
     match Writer::write(&concatenated_path, &result) {
         Ok(()) => println!("Escritura exitosa"),
         Err(e) => eprintln!("Error: {}", e),
